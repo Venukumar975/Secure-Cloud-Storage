73a17abe251521e96a64b46d9ad32f079ce037f6
venu <pichikavenu975@gmail.com>
2026-02-25 01:31:36 +0530

    Added a cusotm AMI

--- main.tf
+++ main.tf
@@ -12,16 +12,64 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my_servers" {
-  count         = 2 # We are telling Terraform to build exactly 2 instances
-  ami           = "ami-0c7217cdde317cfec" # Standard Ubuntu AMI
-  instance_type = "t3.micro"
 
-  tags = {
-    Name = "Server-${count.index + 1}"
+# 1. Create the new Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-server-sg"
+  description = "Allow HTTP and SSH traffic"
+
+  # Inbound rules (Who can connect TO your server)
+  ingress {
+    description = "Allow HTTP (Web Traffic)"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 0.0.0.0/0 means anyone on the internet can see the website
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  # Outbound rules (What the server can connect out to)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+
+
+resource "aws_instance" "my_servers" {
+  count         = 1 # We are telling Terraform to build exactly 2 instances
+  ami           = "ami-008622f29a0929d42" # Standard Ubuntu AMI
+  instance_type = "t3.micro"
+
+  # Attach the Security Group we just created above
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "MyCustomWebUI"
+  }
+}
+
+
+# 3. Print the Public IP so you can easily click it!
+output "website_url" {
+  description = "The public IP address of your Web UI"
+  value       = "http://${aws_instance.my_web_server.public_ip}"
+}
+
+
